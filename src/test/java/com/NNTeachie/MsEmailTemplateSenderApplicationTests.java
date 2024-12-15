538f0dcb0bf105b180f2531f41a4fafc08230de0
NBhanuPrakash <nnsaibhanuprakash@gmail.com>
2024-12-15 14:14:59 +0530

    first commit

---
+++ src/test/java/com/NNTeachie/MsEmailTemplateSenderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.NNTeachie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsEmailTemplateSenderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
